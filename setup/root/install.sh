e4b9af0659a625a8a5085989772add9ce7e618a5
RaNaMaster <RaNaMaster@users.noreply.github.com>
2020-12-27 15:40:01 -0800

    Update install.sh

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,13 +4,14 @@
 set -e
 
 # define pacman packages
-pacman_packages="python2 python2-pip"
+pacman_packages="python3 python3-pip"
 
 # install pre-reqs
 pacman -Sy --needed $pacman_packages --noconfirm
-pip2 install --upgrade setuptools
-pip2 install flexget
-pip2 install transmissionrpc
+pip3 install --upgrade setuptools
+pip3 install flexget
+pip3 install transmissionrpc
+pip3 install youtube-dl
 
 # call aor script (arch official repo)
 # source /root/aur.sh
